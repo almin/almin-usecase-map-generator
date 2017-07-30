5eae74a70e3ca3c96bf6b32c2d51512da2f65650
azu <azuciao@gmail.com>
2017-07-30 17:01:24 +0900

    fix: should throw error when not match files

--- test/almin-usecase-map-generator-test.ts
+++ test/almin-usecase-map-generator-test.ts
@@ -2,7 +2,17 @@
 import { generateUseCaseMap } from "../src/almin-usecase-map-generator";
 import * as assert from "assert";
 
-describe("almin-usecase-map-generator.ts", () => {
+describe("almin-usecase-map-generator", () => {
+    it("should throw error when no match the files", () => {
+        assert.throws(() => {
+            generateUseCaseMap({
+                defaultActor: "AppUser",
+                actors: ["AppUser", "System"],
+                includes: [__dirname + "/not-found-files/use-case/**/*.ts"],
+                format: "nomnoml"
+            })
+        });
+    });
     it("should output nomnoml", () => {
         const output = generateUseCaseMap({
             defaultActor: "AppUser",
